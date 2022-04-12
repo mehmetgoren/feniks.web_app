07df83c6edbad0cffc5ab1198dd7a6a06b810cba
Gokalp Goren <mehmetgoren@outlook.com>
2022-04-12 23:44:37 +0000

    Renames Od files and objects

---
+++ src/components/OdImageGallery.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class='q-pa-md'>
+  </div>
+</template>
+
+<script lang='ts'>
+
+export default {
+  name: 'OdImageGallery',
+  props: {
+    odModel: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
